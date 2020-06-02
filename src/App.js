95804d598c01a6bf8222561297e067ba8f5afce4
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-06-02 20:49:13 +0500

    simple react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello From My React Component
+        </h1>
+    </div>
+  );
+}
+
+export default App;
